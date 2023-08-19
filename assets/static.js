f6b55f521a105dab001ac47e1f1ccf82bfe9fdfe
Apoorv Kansal <apoorvkansalak@gmail.com>
2023-08-19 15:46:21 +0530

    feat:  added auto-updating logic

--- assets/static.js
+++ assets/static.js
@@ -1,56 +1,149 @@
 const optionsEle = document.getElementsByClassName("option");
 
+const POLLS_CONTAINER = document.getElementById("polls-container");
+const POLL_QUESTION_NODES = Array.from(document.getElementsByClassName("poll-container"))
+
+let fetching = false;
+
 const loadingSpinner = document.createElement("span");
 loadingSpinner.classList.add("loading")
+
+const onOptionClick = async (option) => {
+  const pollId = option.dataset.pollId;
+  const optionIndex = option.dataset.optionIndex;
+  const isSubmitted = option.dataset.submitted === "true";
+  if(SUBMIT_QUEUE.includes(pollId) || isSubmitted){
+    return;
+  }
+
+  const clonedLoadingSpinner = loadingSpinner.cloneNode();
+  SUBMIT_QUEUE.push(pollId);
+
+  option.appendChild(clonedLoadingSpinner);
+
+  const { data } = await fetch(`/poll/${pollId}`, {
+    method:"POST",
+    headers: {
+      'content-type':'application/json'
+    },
+    body :JSON.stringify({
+      selectedOption : parseInt(optionIndex,10)
+    })
+  }).then((res) => res.json());
+
+  option.removeChild(clonedLoadingSpinner);
+
+  const queueIndex = SUBMIT_QUEUE.indexOf(pollId);
+
+  SUBMIT_QUEUE.splice(queueIndex, 1);
+  const [option1Count, option2Count] = data;
+
+  const [option1Node , option2Node] = option.parentNode.children;
+
+  const option1Percent = option1Count*100/(option1Count+option2Count)
+
+  option1Node.style.width = `${option1Percent}%`
+  option2Node.style.width = `${100-option1Percent}%`
+
+  option1Node.textContent = option1Node.textContent + `(${option1Percent.toFixed(0)}%)`
+  option2Node.textContent = option2Node.textContent + `(${(100-option1Percent).toFixed(0)}%)`
+
+  option1Node.dataset.submitted = "true"
+  option2Node.dataset.submitted = "true"
+
+  console.log("Submitted Poll",pollId)
+}
 
 const SUBMIT_QUEUE = []
 
 for (let index = 0; index < optionsEle.length; index++) {
   const option = optionsEle[index];
 
-  option.onclick = async () => {
-    const pollId = option.dataset.pollId;
-    const optionIndex = option.dataset.optionIndex;
-    const isSubmitted = option.dataset.submitted === "true";
-    if(SUBMIT_QUEUE.includes(pollId) || isSubmitted){
+  option.onclick = () => onOptionClick(option)
+}
+
+const createPollQuestionNode = (poll, index) => {
+  const pollContainer = document.createElement("div");
+  pollContainer.classList.add("poll-container")
+  pollContainer.dataset.pollId=poll.id
+  
+  const pollQuestion = document.createElement("div")
+  pollQuestion.classList.add("poll-question");
+  pollQuestion.textContent = poll.question
+
+  const optionsContainer = document.createElement("div");
+  optionsContainer.classList.add("poll-options-container");
+
+  const option1Node = document.createElement("div")
+  option1Node.textContent = poll.options[0]
+  option1Node.classList.add("option","option-0")
+  option1Node.dataset.pollId = poll.id;
+  option1Node.data.optionIndex=0
+  option1Node.onclick = () => onOptionClick(option1Node);
+
+  const option2Node = document.createElement("div")
+  option2Node.textContent = poll.options[1]
+  option2Node.classList.add("option","option-1")
+  option2Node.dataset.pollId = poll.id;
+  option2Node.data.optionIndex=1
+  option2Node.onclick = () => onOptionClick(option2Node)
+
+  optionsContainer.appendChild(option1Node)
+  optionsContainer.appendChild(option2Node)
+
+  pollContainer.appendChild(pollQuestion)
+  pollContainer.appendChild(optionsContainer);
+
+  POLLS_CONTAINER.append(pollContainer);
+
+  POLL_QUESTION_NODES[index] = pollContainer;
+}
+
+setInterval(() => {
+ (async () => {
+    if(SUBMIT_QUEUE.length > 0 || fetching){
       return;
     }
+    fetching = true;
+    let updatedPolls;
+    try {
+      updatedPolls = await fetch("/polls").then(res => res.json());
+    } catch (error) {
+      window.location.reload()// reload 
+    }
 
-    const clonedLoadingSpinner = loadingSpinner.cloneNode();
-    SUBMIT_QUEUE.push(pollId);
+    updatedPolls.forEach((poll, index) => {
+      const pollNode = POLL_QUESTION_NODES[index];
+      // Don't update if not submitted and node is already there
+      if(!poll.submitted && pollNode){
+        return;
+      }
 
-    option.appendChild(clonedLoadingSpinner);
+      if(!pollNode){
+        createPollQuestionNode(poll, index)
+      }
 
-    const { data } = await fetch(`/poll/${pollId}`, {
-      method:"POST",
-      headers: {
-        'content-type':'application/json'
-      },
-      body :JSON.stringify({
-        selectedOption : parseInt(optionIndex,10)
-      })
-    }).then((res) => res.json());
+      // updaete node
+      const [, optionsContainer]=pollNode.children;
 
-    option.removeChild(clonedLoadingSpinner);
+      const [option1Node, option2Node] = optionsContainer.children;
 
-    const queueIndex = SUBMIT_QUEUE.indexOf(pollId);
+      const [option1Count, option2Count] = poll.submissions;
 
-    SUBMIT_QUEUE.splice(queueIndex, 1);
-    const [option1Count, option2Count] = data;
+      const option1Percent = option1Count*100/(option1Count+option2Count)
 
-    const [option1Node , option2Node] = option.parentNode.children;
+      option1Node.style.width = `${option1Percent}%`
+      option2Node.style.width = `${100-option1Percent}%`
 
-    const option1Percent = option1Count*100/(option1Count+option2Count)
+      option1Node.textContent = poll.options[0] + `(${option1Percent.toFixed(0)}%)`
+      option2Node.textContent = poll.options[1] + `(${(100-option1Percent).toFixed(0)}%)`
 
-    option1Node.style.width = `${option1Percent}%`
-    option2Node.style.width = `${100-option1Percent}%`
+      option1Node.dataset.submitted = "true"
+      option2Node.dataset.submitted = "true"
 
-    option1Node.textContent = option1Node.textContent + `(${option1Percent.toFixed(0)}%)`
-    option2Node.textContent = option2Node.textContent + `(${(100-option1Percent).toFixed(0)}%)`
+    })
 
-    option1Node.dataset.submitted = "true"
-    option2Node.dataset.submitted = "true"
+    fetching=false;
 
-    console.log("Submitted Poll",pollId)
-  }
-}
+ })()
+},750 + Math.floor(Math.random()*50)) // add a random value so all the client send api request at diffrerent times
